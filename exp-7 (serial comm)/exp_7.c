c4a8da4d3084d64280596e30af6989dc0c6d5d27
Sun Tzu <itsukonagai20@gmail.com>
2025-11-13 00:54:24 +0530

    Serial communication

--- exp-7 (serial comm)/exp_7.c
+++ exp-7 (serial comm)/exp_7.c
@@ -1,46 +1,50 @@
-#include <P18F4520.h> 
+#include <P18F4520.h>
+
 #pragma config OSC = HS    
 #pragma config WDT = OFF   
 #pragma config LVP = OFF    
-#pragma config PBADEN = OFF  
+#pragma config PBADEN = OFF   
 
-void Transmit_String(unsigned char *string) 
-{ 
-} 
-unsigned char i = 0; 
-for(i = 0; string[i] != '\0'; i++)  
-{ 
-} 
-while(PIR1bits.TXIF == 0);     
-TXREG = string[i];            
-unsigned char MSG1[] = ("UART COMMUNICATION \r\n"); 
-unsigned char MSG2[] = ("TRANSMITTING STRING \r\n"); 
-unsigned char MSG3[] = ("SEND 10 Characters \r\n"); 
-unsigned char MSG4[] = ("Received Data \r\n"); 
-void main(void) 
+void Transmit_String(unsigned char *string) {
+    unsigned char i = 0;
+    while (string[i] != '\0') {
+        while (PIR1bits.TXIF == 0);  // Wait until transmit buffer is empty
+        TXREG = string[i];           // Load character into transmit register
+        i++;
+    }
+}
 
-{ 
-unsigned char j = 0; 
-unsigned char RX_DATA[20]; 
-unsigned char MSG5[] = ("Received Data \r\n"); 
-TRISCbits.TRISC7 = 1;  
-TRISCbits.TRISC6 = 0;   
-RCSTA = 0x90;   
-TXSTA = 0x24;    
-BAUDCON = 0x00;  
-SPBRG = 0x19;    
-SPBRGH = 0x00; 
-Transmit_String(MSG1); 
-Transmit_String(MSG2); 
-Transmit_String(MSG3); 
-for(j = 0; j < 10; j++) 
-{ 
-} 
-while(PIR1bits.RCIF == 0);   
-RX_DATA[j] = RCREG;      
+unsigned char MSG1[] = "UART COMMUNICATION \r\n";
+unsigned char MSG2[] = "TRANSMITTING STRING \r\n";
+unsigned char MSG3[] = "SEND 10 Characters \r\n";
+unsigned char MSG4[] = "Received Data \r\n";
 
-RX_DATA[10] = '\0';   
-Transmit_String(MSG4); 
-Transmit_String(RX_DATA); 
-while(1);  
-} 
+void main(void) {
+    unsigned char j = 0;
+    unsigned char RX_DATA[20];
+
+    TRISCbits.TRISC7 = 1;   // RX pin as input
+    TRISCbits.TRISC6 = 0;   // TX pin as output
+
+    RCSTA = 0x90;           // Enable serial port and continuous receive
+    TXSTA = 0x24;           // Enable transmitter, set BRGH
+    BAUDCON = 0x00;         // Baud rate control
+    SPBRG = 0x19;           // Baud rate low byte
+    SPBRGH = 0x00;          // Baud rate high byte
+
+    Transmit_String(MSG1);
+    Transmit_String(MSG2);
+    Transmit_String(MSG3);
+
+    for (j = 0; j < 10; j++) {
+        while (PIR1bits.RCIF == 0);  // Wait until data is received
+        RX_DATA[j] = RCREG;          // Read received character
+    }
+
+    RX_DATA[10] = '\0';              // Null-terminate the string
+
+    Transmit_String(MSG4);
+    Transmit_String(RX_DATA);
+
+    while (1);  // Infinite loop
+}
